980cbf5ea62e4cede8ed4c15ad5d4dd0526953e9
Gustavo Cerqueira <gmcerqueira93@gmail.com>
2021-09-28 17:33:44 -0300

    create UserController class

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -8,22 +8,24 @@ const jwtConfig = {
   algorithm: 'HS256',
 };
 
-const singUp = async (req, res) => {
-  const userCreate = await UserService.createNewUser(req.body);
+class UserController {
+  async singUp(req, res) {
+    const userCreate = await UserService.createNewUser(req.body);
 
-  if (!userCreate) return res.status(409).json({ message: 'This email is already register' });
+    if (!userCreate) return res.status(409).json({ message: 'This email is already register' });
 
-  return res.status(200).json({ userId: userCreate });
-};
+    return res.status(200).json({ userId: userCreate });
+  }
 
-const login = async (req, res) => {
-  const logged = await UserService.verifyLogin(req.body);
+  async login(req, res) {
+    const logged = await UserService.verifyLogin(req.body);
 
-  if (!logged) return res.status(409).json({ message: 'Email or password incorrect' });
+    if (!logged) return res.status(409).json({ message: 'Email or password incorrect' });
 
-  const token = jwt.sign(logged, secret, jwtConfig);
+    const token = jwt.sign(logged, secret, jwtConfig);
 
-  return res.status(200).json({ token });
-};
+    return res.status(200).json({ token });
+  }
+}
 
-export { singUp, login };
+export default new UserController();
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
-import TaskController from '../controllers/task.controller';
-import { singUp, login } from '../controllers/user.controller';
+import Task from '../controllers/task.controller';
+import User from '../controllers/user.controller';
 import validateJWT from '../middlewares/jwtAuth';
 
 const routes = new Router();
@@ -9,10 +9,10 @@ routes.get('/', (_req, res) => {
   res.status(200).json({ ok: 'connected' });
 });
 
-routes.get('/tasks', validateJWT, TaskController.getAll);
-routes.post('/tasks/create', validateJWT, TaskController.newTask);
+routes.get('/tasks', validateJWT, Task.getAll);
+routes.post('/tasks/create', validateJWT, Task.newTask);
 
-routes.post('/singup', singUp);
-routes.post('/login', login);
+routes.post('/singup', User.singUp);
+routes.post('/login', User.login);
 
 export default routes;
