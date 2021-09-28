e583c40172096ce5f15eefd81d1c954c3495e8c8
Gustavo Cerqueira <gmcerqueira93@gmail.com>
2021-09-28 17:14:32 -0300

    create TaskModel class

--- src/models/task.model.js
+++ src/models/task.model.js
@@ -1,20 +1,22 @@
 import connection from './connection';
 
-const findAll = async (userId) => {
-  const db = await connection();
+class TaskModel {
+  async findAll(userId) {
+    const db = await connection();
 
-  const list = await db.collection('tasks').find({ userId }).toArray();
+    const list = await db.collection('tasks').find({ userId }).toArray();
 
-  return list;
-};
+    return list;
+  }
 
-const create = async (text, userId) => {
-  const db = await connection();
-  const task = await db
-    .collection('tasks')
-    .insertOne({ text, userId, created: new Date() });
+  async create(text, userId) {
+    const db = await connection();
+    const task = await db
+      .collection('tasks')
+      .insertOne({ text, userId, created: new Date() });
 
-  return task;
-};
+    return task;
+  }
+}
 
-export { findAll, create };
+export default new TaskModel();
--- src/services/task.service.js
+++ src/services/task.service.js
@@ -1,15 +1,15 @@
-import { findAll, create } from '../models/task.model';
+import taskModel from '../models/task.model';
 
 const listTasks = async (user) => {
   const { _id } = user;
-  const list = await findAll(_id);
+  const list = await taskModel.findAll(_id);
 
   return list;
 };
 
 const registerTask = async (text, user) => {
   const { _id } = user;
-  const task = await create(text, _id);
+  const task = await taskModel.create(text, _id);
 
   return task;
 };
