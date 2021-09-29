0d95a0ce91502ff156044eeb15cfb272ec64432f
Gustavo Cerqueira <gmcerqueira93@gmail.com>
2021-09-29 20:35:18 -0300

    build

--- build/routes/index.js
+++ build/routes/index.js
@@ -1,4 +1,5 @@
-"use strict";Object.defineProperty(exports, "__esModule", {value: true}); function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _express = require('express');
+"use strict";Object.defineProperty(exports, "__esModule", {value: true}); function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _cors = require('cors'); var _cors2 = _interopRequireDefault(_cors);
+var _express = require('express');
 
 
 
@@ -11,6 +12,8 @@ var _jwtAuth = require('../middlewares/jwtAuth'); var _jwtAuth2 = _interopRequir
 var _erros = require('../middlewares/erros'); var _erros2 = _interopRequireDefault(_erros);
 
 const routes = new (0, _express.Router)();
+
+routes.use(_cors2.default.call(void 0, ));
 
 routes.get('/', (_req, res) => {
   res.status(200).json({ connection: true });
--- build/server.js
+++ build/server.js
@@ -1,6 +1,6 @@
 "use strict"; function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _app = require('./app'); var _app2 = _interopRequireDefault(_app);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 _app2.default.listen(port, () => {
   console.log(`Listening in port ${port}`);
